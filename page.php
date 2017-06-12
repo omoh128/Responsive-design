23b4718a92bdf903839b20ad12c05d39ec1685a3
omomoh <omoh128@gmail.com>
2017-06-12 08:02:36 +0200

    Create page.php

--- page.php
+++ page.php
@@ -1,49 +1,46 @@
-<HTML>
+<html>
 <HEAD>
 <TITLE>Responsive Web Design</TITLE>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="style.css" rel="stylesheet" type="text/css" />
 </HEAD>
-<BODY>
-          
-
+      <body>
          <div id="page-content">
-          
-           <div id="responsive-menu">
-			<div id="menu-expand-collapse"><img src="menu.png" width="25px"></div>
-			 <div id="responsive-menu-list">
-				 <div class="menu-item">
-					 <a href="#" >Home</a>
-				 </div>
-				 <div class="menu-item">
-					 <a href="#" >About</a>
-				 </div>
-				 <div class="menu-item">
-					 <a href="#">Services</a>
-				 </div>	 
-				 <div class="menu-item">
-					<a href="#">Contact</a>
-				 </div>			 
-			 </div>		 
-    </div>
-           <?php
+                <div id="responsive-menu">
+		  <div id="menu-expand-collapse"><img src="menu.png" width="25px"></div>
+		    <div id="responsive-menu-list">
+			<div class="menu-item">
+			  <a href="#" >Home</a>
+			</div>
+			 <div class="menu-item">
+			   <a href="#" >About</a>
+			</div>
+			  <div class="menu-item">
+			   <a href="#">Services</a>
+			</div>	 
+			 <div class="menu-item">
+			   <a href="#">Contact</a>
+			</div>			 
+		   </div>		 
+              </div>
+         
          include_once("header.php");
-                 ?>
-        <?php
+                
+       
         include("content.php");
-           ?>
-         <?php
+       
+        
       include("footer.php");
-             ?>
+        
         </div>
-        </div>
-<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+     
+      <script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
 		<script>
 		$(document).ready(function(){
 			$("#menu-expand-collapse").click(function() {
 				$("#responsive-menu-list").toggle();
 			});
 		});
-		</script>
-</BODY>
-</HTML>
+      </script>
+   </body>
+</html>
